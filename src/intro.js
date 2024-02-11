9f0644214a00c6d0e118599c4a65fa4cc4f18c8e
Hakan Berke Temur <hkn2407@gmail.com>
2024-02-11 15:44:39 +0300

    initial commit

---
+++ src/intro.js
@@ -0,0 +1,630 @@
+// const hakan = "Hakan Berke Temur";
+
+// hakan = "berke temur";
+// console.log(hakan);
+
+// let instructionName = "hakan";
+// instructionName = "Hakan Temur";
+// console.log(instructionName);
+
+// console.log("Hakan temur");
+
+// console.log(2024);
+// console.log(6734);
+
+// let mert = false;
+// console.log(mert);
+// console.log("bşr varmıs jhasjfjdafh sıfksfhks k hskfh \"kahfsdasfhskafhkahfskhfsak\".");
+
+
+// const sentence = "dfhkdfdkhafhfahahdhafhfdhfhdahfh";
+
+// console.log(sentence.length);
+
+// console.log(sentence[10]);
+// const name1 = "hakan berke";
+// const name2 = "temur";
+// const name3 = 12345;
+
+// console.log(typeof name1, typeof name2, typeof name3);
+
+// let yas;
+// console.log(yas);
+
+// let buyuk = null;
+// console.log(buyuk);
+
+// const sentence = "bebegim oldun ilk bebegim günden beri iyi ki hayatımdasın";
+
+// console.log(sentence.indexOf("bebegim",1));
+
+// console.log(sentence.slice(8, 14));
+// console.log(sentence.slice(15, -5));
+
+// console.log(sentence.substring(15));
+
+
+
+// let name = "Hakan Berke temur";
+// let laguage = "JavaScript";
+// let kurs = "Frontend Web Developer";
+// let tarih = "04.10.2022";
+
+// console.log("");
+// console.log(Math.random() * 20);
+
+
+// console.log(Math.ceil(Math.random() * 100));
+// console.log(Math.floor(Math.random() * 100));
+
+// console.log(Math.ceil(Math.random() * 17));
+// console.log(Math.floor(Math.random() * 17));
+
+// console.log(Math.ceil(Math.random() *10)-5);
+// console.log(Math.floor(Math.random() *10)-5);
+
+// console.log(Math.ceil(Math.random() *32)-32);
+// console.log(Math.floor(Math.random() *32)-32);
+
+// let age = 66;
+
+// if (age < 18) {
+//     console.log("underaged");
+// }
+// else if (age >= 65) {
+//     console.log("senior");
+// }
+// else if (age >= 18) {
+//     console.log("young adult");
+// }
+// else {
+//     console.log("cannot pass");
+// }
+
+
+// if (age < 18) {
+//     console.log("underaged");
+// }
+// else if (age <= 64) {
+//     console.log("young adult");
+// }
+// else if (age >= 65) {
+//     console.log("senior");
+// }
+// else {
+//     console.log("cannot pass");
+// }
+
+
+// let firstName = "Hakan";
+
+// if (firstName == "Hakan"){
+//     console.log("Hakan Berke Temur");
+// }
+// else {
+//     console.log("Error");
+// }
+
+
+//ternary operator
+// age > 18 ? console.log("adult") : console.log("underaged"); //expression
+// if (age > 18) { console.log("adult"); } else { console.log("underaged"); } //statement
+
+// let ageBar = if (age > 18) { return "adult" } else { return "underaged" };
+// let ageBar = age > 18 ? "adult" : "underaged";
+
+// console.log(age > 18 ? "adult" : "underaged");
+
+// let yas = 34;
+
+// if (yas > 40){
+//     console.log("Yaşınız 40'tan büyük değil....");
+// }
+// else {
+//     console.log("Yaşınız 40'tan küçük...");
+// }
+
+// let sayi1 = 55;
+// let sayi2 = 10;
+
+// if (sayi1 <= sayi2){
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// let number = 15;
+
+// for (i = 0; i <= 5; i++)
+// {
+//     console.log(number);
+//     number++;
+// }
+
+// let animal = "Köpek";
+// let animal2 = "kedi";
+
+// animal === "Köpek" ? console.log("evet doğru bildiniz...") : console.log("hayır yanlış bilgi...");
+
+// console.log(animal);
+
+// let yas = 33;
+
+// yas > 30 && console.log("hakan");
+
+
+// let firstName = "berke";
+
+// firstName === "berke" && console.log("temur");
+
+// let sayi = 11;
+
+// sayi > 55 || console.log("11 5'ten büyük");
+
+// let sayi1 = 11;
+
+// !sayi1 > 5 || console.log("11 5'ten büyüktür.....");
+
+
+// let gun = 6;
+
+// switch (gun) {
+//     case 1:
+//         console.log("Pazartesi");
+//         break;
+//     case 2:
+//         console.log("Salı");
+//         break;
+//     case 3:
+//         console.log("Çarşamba");
+//         break;
+//     case 4:
+//         console.log("Perşembe");
+//         break;
+//     case 5:
+//         console.log("Cuma");
+//         break;
+//     case 6:
+//         console.log("Cumartesi");
+//         break;
+//     case 7:
+//         console.log("Pazar");
+//         break;
+//     default:
+//         console.log("Lütfen doğru bir değer girin...");
+//         break;
+// }
+
+// let day = 1;
+
+// if (day === 1) {
+//     console.log("Pazartesi");
+// }
+// else if (day === 2) {
+//     console.log("Salı");
+// }
+// else if (day === 3) {
+//     console.log("Çarşamba");
+// }
+// else if (day === 4) {
+//     console.log("Perşembe");
+// }
+// else if (day === 5) {
+//     console.log("Cuma");
+// }
+// else if (day === 6) {
+//     console.log("Cumartesi");
+// }
+// else if (day === 72) {
+//     console.log("Pazar");
+// }
+// else {
+//     console.log("yanlış gridiniz...");
+// }
+
+// let month = -12;
+
+// month = month % 12;
+
+// switch (Math.abs(month)) {
+//     case 1:
+//         console.log("Ocak");
+//         break;
+//     case 2:
+//         console.log("Şubat");
+//         break;
+//     case 3:
+//         console.log("Mart");
+//         break;
+//     case 4:
+//         console.log("Nisan");
+//         break;
+//     case 5:
+//         console.log("Mayıs");
+//         break;
+//     case 6:
+//         console.log("Haziran");
+//         break;
+//     case 7:
+//         console.log("Temmuz");
+//         break;
+//     case 8:
+//         console.log("Ağustos");
+//         break;
+//     case 9:
+//         console.log("Eylül");
+//         break;
+//     case 10:
+//         console.log("Ekim");
+//         break;
+//     case 11:
+//         console.log("Kasım");
+//         break;
+//     case 12:
+//         console.log("Aralık");
+//         break;
+
+//     default:
+//         console.log("Lütfen doğru bir değer giriniz....");
+//         break;
+// }
+
+// let ay = 1;
+
+// if (ay === 1) {
+//     console.log("Ocak");
+// }
+// else if (ay === 2) {
+//     console.log("Şubat");
+// }
+// else if (ay === 3) {
+//     console.log("Mart");
+// }
+// else if (ay === 4) {
+//     console.log("Nisan");
+// }
+// else if (ay === 5) {
+//     console.log("Mayıs");
+// }
+// else if (ay === 6) {
+//     console.log("Haziran");
+// }
+// else if (ay === 7) {
+//     console.log("Temmuz");
+// }
+// else if (ay === 8) {
+//     console.log("Ağustos");
+// }
+// else if (ay === 9) {
+//     console.log("Eylül");
+// }
+// else if (ay === 10) {
+//     console.log("Ekim");
+// }
+// else if (ay === 11) {
+//     console.log("Kasım");
+// }
+// else if (ay === 12) {
+//     console.log("Aralık");
+// }
+// else {
+//     console.log("lütfen doğru giriniz...");
+// }
+
+// let kacinciGun = 7;
+
+// sonuc = kacinciGun % 7;
+
+// if(sonuc === 1){
+//     console.log("Haftanın ilk günü yani Pazartesi");
+// }
+// else if (sonuc === 2) {
+//     console.log("Haftanın ikinci günü yani Salı");
+// }
+// else if (sonuc === 3) {
+//     console.log("Haftanın üçüncü günü yani Çarşamba");
+// }
+// else if (sonuc === 4) {
+//     console.log("Haftanın dördüncü günü yani Perşembe");
+// }
+// else if (sonuc === 5) {
+//     console.log("Haftanın beşinci günü yani Cuma");
+// }
+// else if (sonuc === 6) {
+//     console.log("Haftanın altıncı günü yani Cumartesi");
+// }
+// else if (sonuc === 7) {
+//     console.log("Haftanın yedinci günü yani Pazar");
+// }
+// else{
+//     console.log("Yanlış girdi...");
+// }
+
+// let sayi = "HAKAN, BERKE TEMUR";
+// let bul = sayi.length;
+// let ilk = 0;
+
+// while (ilk < bul) {
+//     if (sayi.charAt(ilk) === " " || sayi.charAt(ilk) === ","){
+//         console.log(sayi.replace(""));
+//     }
+//     console.log(sayi.charAt(ilk));
+//     ilk++;
+// }
+
+// const sentence = "Mert erken yat, programlama calis!";
+// let index = 0;
+
+// while (index < sentence.length) {
+//     if (sentence[index] === " " || sentence[index] === ",") {
+//         index++;
+//         continue;
+//     }
+//     console.log(sentence[index]);
+//     index++;
+// }
+
+// const num1 = [1,2,3];
+// const num2 = [4,5,6];
+// const nums =num1.concat(num2);
+// console.log(nums);
+
+// const firstNames = ["ali", "ayşe", "sude", "furkan"];
+
+// console.log(firstName.length);
+
+// const secondName = ["mahmut", "ilker"];
+// const birlestir = firstNames.concat(secondName);
+// console.log(birlestir);
+
+// const nums = [1,2,3,7,5,6];
+// nums.shift();
+// nums.pop();
+// nums.push(10);
+// nums.unshift(20)
+// let bul = nums.indexOf(5)
+// let string = nums.join("-");
+// console.log(nums.toString());
+// console.log(bul);
+// console.log(string);
+
+// let yeni = nums.includes(nums.indexOf(5));
+// console.log(yeni);
+// firstNames.includes(firstNames.indexOf(3));
+
+// let deger = nums.indexOf(7)
+
+// if (deger === -1){
+//     console.log(false);
+// }else {
+//     console.log(true);
+// }
+
+
+//splice
+
+// const products = [
+//     "Elma", 30, true,
+//     "Armut", 40, true,
+//     "Kivi", 43, false,
+//     "Mandalina", 27,true,
+//     "Muz", 50,false,
+//     "Nar", 25, false,
+//     "Portakal", 35, false,
+//     "Üzüm", 45, true,
+// ]
+
+//urun ekleme
+// products.splice(24, 0, "Ananas", 65, true);
+// products.splice(27, 0, "Avakado", 60, false);
+// products.splice(30, 0, "Çilek", 20, true);
+// products.splice(33, 0, "Şeftali", 55, true);
+
+//urun silme
+// products.splice(3, 3);
+// products.splice(12, 3);
+// products.splice(0, 3);
+// products.splice(21, 3);
+
+//urun gucelleme
+// products.splice(13, 2, 70, true);
+// products.splice(19, 2, 90, true);
+// products.splice(7, 2, 62, true);
+// products.splice(22, 2, 40, false);
+
+//4 islem aynı anda yapma
+// products.splice(0, )
+
+//indexof la urun guncelleme
+
+// console.log(products);
+
+// const firstNames = ["ali", "hakan", "berke", "temur", "barkın", "çağatay", "mert", "onur", "berkay", "mahmut", "furkan"];
+// const userName = prompt("Bir isim girin: ").trim().toLowerCase();
+// let result = null;
+// for (let i = 0; i < firstNames.length; i++) {
+//     if (firstNames[i] === userName) {
+//         result = firstNames[i];
+//         break;
+//     }
+// }
+// if (result === userName) {
+//     console.log("Aradığınız değer burada mevcut...");
+// }
+// else {
+//     console.log("Aradığınınz değer burada mevcut değil...");
+// }
+
+// function toplama (num1, num2) {
+//     const sum = num1 + num2;
+//     return sum;
+// }
+// let sonuc = toplama(22, 44)
+// console.log(sonuc);
+
+// console.log(sqrt(25, 5));
+// function sqrt(num1, num2){
+//     const result = Math.sqrt(num1, num2);
+//     return result;
+// }
+
+// function tekCift(nums) {
+//     if (nums % 2 === 0){
+//         console.log("Sayınız Çifttir...");
+//     }
+//     if (nums % 2 === 1) {
+//         log
+//     }
+// }
+
+// const carpim = function (num1, num2) {
+//     const result = num1 * num2;
+//     return result;
+// }
+// console.log(carpim(5, 4));
+
+// const bolum = function (num1 , num2) {
+//     const result = num1 / num2;
+//     return result;
+// }
+// console.log(bolum(12, 3));
+
+// const cikarma = (num1 = 0 , num2 = 0) => num1 - num2;
+// const text = () => console.log("herkese merhabalar...");
+// const firstNames = names => console.log("hakan berke" + names);
+
+// console.log(cikarma(10, 5));
+// console.log(cikarma());
+// console.log(cikarma(10));
+// console.log(text());
+// console.log(firstNames("temur"));
+
+
+//quiz
+
+// const  numbers = [1,2,3,4,5,6,7,8,9,10];
+// const firstNames = ["hakan", "berke", "temur"];
+// const randomContents = ["hakan berke ", 1,2,4,false];
+// const animals = ["dog","elephant","cat"];
+// const carBrands = ["bmw","audi","mercedes","ford"];
+
+// function Arr(array) {
+//     let newArray = []
+//     for (let i = array.length - 1; i >= 0; i--) {       
+//         newArray[newArray.length] = array[i];
+//     }
+//     return newArray;
+// }
+
+// console.log(Arr(numbers));
+// console.log(Arr(firstNames));
+// console.log(Arr(randomContents));
+// console.log(Arr(animals));
+// console.log(Arr(carBrands));
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const firstNames = ["hakan", "berke", "temur"];
+// const randomContents = ["hakan berke ", 1, 2, 4, false];
+// const animals = ["dog", "elephant", "cat"];
+// const carBrands = ["bmw", "audi", "mercedes", "ford"];
+
+// const Arr = (array) => {
+//     let newArray = [];
+//     for (let i = array.length - 1; i >= 0; i--) {
+//         newArray[newArray.length] = array[i];
+//     }
+//     return newArray;
+// }
+
+// console.log(Arr(numbers));
+// console.log(Arr(firstNames));
+// console.log(Arr(randomContents));
+// console.log(Arr(animals));
+// console.log(Arr(carBrands));
+
+// const person = {
+//     name: "mehmet",
+//     age: 44
+// }
+
+// let promptName = person;
+
+// console.log(promptName);
+
+// console.log(person);
+
+// ÖNEMLİ TEKRAR EDİLECEK
+// const student1 = {
+//     //key : value
+//     fullName: "Berkay Turna",
+//     "nickname": "chilgin_coder",
+//     //kebab-case
+//     "is-active": true,
+//     1: "100",
+//     logValues: function () {
+//         console.log(this.fullName);
+//     },
+// };
+// let propName = "fullName";
+// console.log(student1["fullName"]);
+// propName = "1";
+// console.log(
+//     student1[propName]
+// );
+
+const car = {
+    make: [
+        { make1: "bmw", model1: "m5" },
+        { make2: "audi", model2: "s8" },
+        { make3: "ford", model3: "focus" }],
+    color: ["blue", "red", "white", "black", "grey"],
+    year: 2024,
+    country: "germany",
+    numberDoors: ["2doors", "4doors"],
+    spoiler: false,
+}
+
+car.spoiler = true;
+car.manuel = false;
+
+console.log(Object.keys(car));
+console.log(car["make"][2].hasOwnProperty("make3"));
+console.log(Object.values(car));
+console.log(Object.entries(car));
+console.log(car.make);
+console.log(car);
+
+
+const phone = {
+    make: ["iphone11", "iphone12", "iphone13"],
+    year: 2022,
+    depolama: [
+        { depo: "128GB", icloud: "5GB" },
+        { depo: "256GB", icloud: "50GGB" },
+        { depo: "512GB", icloud: "100GB" }],
+    color: "white",
+    sarjkablosu: true
+}
+
+console.log(Object.keys(phone));
+console.log(phone["depolama"][1].hasOwnProperty("depo"));
+console.log(Object.values(phone));
+console.log(Object.entries(phone));
+console.log(phone.depolama);
+console.log(phone);
+
+const city = {
+    cities: ["istanbul", "ankara", "eskişehir", "muğla", "izmir"],
+    ilce: [{ ist: "kadıköy", ank: "sincan", esk: "merkez", mug: "bodrum", izmir: "bornova" }],
+    plaka: [34, 6, 26, 48, 35],
+    havaalani: true,
+    deniz: [{ istan: true }, { izm: true }, { anka: false }]
+}
+
+console.log(Object.keys(city));
+console.log(city["deniz"][0].hasOwnProperty("istan"));
+console.log(Object.values(city));
+console.log(Object.entries(city));
+console.log(city.cities);
+console.log(city);
