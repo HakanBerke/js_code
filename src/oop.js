0f92bf6f6557ecbf518029fc71602274a9304acd
Hakan Berke Temur <hkn2407@gmail.com>
2024-02-27 19:43:04 +0300

    oops-reference

---
+++ src/oop.js
@@ -0,0 +1,38 @@
+"use strict";
+// function Animals(cinsi, ayakSayisi, cinsiyeti, kanati) {
+//     this.cinsi = cinsi;
+//     this.ayakSayisi = ayakSayisi;
+//     this.cinsiyeti = cinsiyeti;
+//     this.kanati = kanati;
+//     Object.seal(this);
+// }
+
+// Animals.prototype.tuyRengi 
+
+// const animal1 = new Animals("Köpek",4,"E",false);
+// const animal2 = new Animals("kuş",2,"E",true);
+// const animal3 = new Animals("kedi",4,"K",false);
+// const animal4 = "abcsdas";
+// const animals =[]
+
+// animal1.kuyruk = true;
+// animal1.cinsiyeti= "K";
+// console.log(animal1);
+// console.log(animal2);
+// console.log(animal3);
+
+// function ekleme (obj){
+//     if (obj instanceof Animals){
+//         animals.push(obj)
+//     }
+//     else {
+//         throw "yanlış...."
+//     }
+// }
+// ekleme(animal1);
+// console.log(animals);
+
+// class Car {
+//     carName;
+//     car
+// }
