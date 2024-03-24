72f32c7e736b3841d1baee56ec5da547ccd3caa0
Hakan Berke Temur <hkn2407@gmail.com>
2024-03-24 12:06:23 +0300

    react-form

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -2,36 +2,59 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [studentName1, setStudentName1] = useState("Hakan Berke Temur");
-  const [studentName2, setStudentName2] = useState("Barkın Sayın");
-  const [studentName3, setStudentName3] = useState("Berkay Turna");
+
+  //data state
+  const [studentName, setStudentName] = useState("");
+  const [course, setCourse] = useState("");
+  const [instructor, setInstructor] = useState("");
+
+  //input state
+  const [studentNameInput, setStudentNameInput] = useState("");
+  const [courseInput, setCourseInput] = useState("");
+  const [instructorInput, setInstructorInput] = useState("");
   return (
+    /* JSX */
     <div className="app">
       <h2>Student Manager</h2>
-      <p>{studentName1}</p>
-      <button
-      onClick={
-        () => {
-         setStudentName1("cenk kaynak")}}
-      >
-         Change Student Name
-      </button>
-      <p>{studentName2}</p>
-      <button
-      onClick={
-        () => {
-         setStudentName2("Onur ege")}}
-      >
-         Change Student Name
-      </button>
-      <p>{studentName3}</p>
-      <button
-      onClick={
-        () => {
-         setStudentName3("Ali Rıza")}}
-      >
-         Change Student Name
-      </button>
+      <form>
+        {/* two way binding */}
+        <input type="text" placeholder='Student Name' value={studentNameInput} onChange={(event) => setStudentNameInput(event.target.value)} />
+        <br /><br />
+        <input type="text" placeholder='Course' value={courseInput} onChange={(event) => setCourseInput(event.target.value)} />
+        <br /><br />
+        <input type="text" placeholder='Instructor' value={instructorInput} onChange={(event) => setInstructorInput(event.target.value)} />
+        <br /><br />
+        <input
+          type="submit"
+          onClick={
+            (event) => {
+              event.preventDefault();
+              if (studentNameInput.trim() === "" || courseInput.trim() === "" || instructorInput.trim() === "") {
+                alert("Please fill all the fields");                
+              }else {
+                setStudentName(studentNameInput.trim());
+                setCourse(courseInput.trim());
+                setInstructor(instructorInput.trim());
+
+                //inputlari temizle
+                setStudentNameInput("");
+                setCourseInput("");
+                setInstructorInput("");
+              }
+            }
+          }
+        />
+      </form>
+      <div className="student-list">
+        <h3>Students</h3>
+        <div className="student-card">
+          <ul>
+            <li>{studentName}</li>
+            <li>{course}</li>
+            <li>{instructor}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
