3cf21e0fcb5d9fa0c3b6f12da4cb84c4500c4d6b
Hakan Berke Temur <hkn2407@gmail.com>
2024-03-31 13:07:46 +0300

    stateerror-prevstudent

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -3,28 +3,81 @@ import { useState } from 'react';
 
 function App() {
 
-  //data state
-  const [studentName, setStudentName] = useState("");
-  const [course, setCourse] = useState("");
-  const [instructor, setInstructor] = useState("");
+  const [studentList, setStudentList] = useState(
+
+    [
+      { studentName: "Barkin Onay Sayin", course: "Onay Teknikleri", instructor: "Hicran Ertugral", id: "001" },
+      { studentName: "Ali Riza Taskiran", course: "JavaScript", instructor: "Buse Ugras", id: "002" },
+      { studentName: "Berkay Turna", course: "React", instructor: "Orun Durmaz", id: "003" },
+      { studentName: "Cenk Kaynak", course: "Html Css", instructor: "Orkun Durmaz", id: "004" },
+    ]
+  );
 
   //input state
-  const [studentNameInput, setStudentNameInput] = useState("");
-  const [courseInput, setCourseInput] = useState("");
-  const [instructorInput, setInstructorInput] = useState("");
+  const [student, setStudent] = useState({ studentName: "", course: "", instructor: "" });
 
   //error states
-  const [studentNameError, setStudentNameError] = useState(false);
-  const [courseError, setCourseError] = useState(false);
-  const [instructorError, setInstructorError] = useState(false);
+  const [studentError, setStudentError] = useState({ studentName: false, course: false, instructor: false })
 
-  //her veriyi yeni gösterme 
-  const [student, setStudent] = useState([]);
-  const [studentList, setstudentList] = useState([
-    { studentName: "Hakan Berke", course: "js", instructor: "orkun durmaz", id: "1" },
-    { studentName: "barkın sayın", course: "react", instructor: "hicran ertuğral", id: "2" },
-    { studentName: "cenk kaynak", course: "grid", instructor: "önder tarım", id: "3" },
-    { studentName: "ali rıza", course: "html-css", instructor: "orkun durmaz", id: "4" }])
+  const addStudent = (event) => {
+    event.preventDefault();
+
+    //clear previous errors
+    setStudentError({ studentName: false, course: false, instructor: false })
+
+    if (!student.studentName.trim() || !student.course.trim() || !student.instructor.trim()) {
+      setStudentError(
+        {
+          studentName: !student.studentName.trim(),
+          course: !student.course.trim(),
+          instructor: !student.instructor.trim()
+        }
+      )
+      //else yerine early return pattern kullanilabilir
+      return;
+    }
+
+    //studentList'i guncelle
+    // const newStudent = { studentName: "Ceren Dincer", course: "Node.js", instructor: "Orkun Durmaz", id: Math.random().toString() };
+    //yanlis!!!!  side effect yani yan etki oldugu icin calisiyor gibi gozukuyor
+    // studentList.push(newStudent);
+
+    //YANLISS!!!!! DIZI BOYLE KOPYALANMAZ!!!!!
+    // const newStudentList = studentList;
+    // newStudentList.push(newStudent);
+    // //ADRESLER AYNI OLDUGU ICIN STATE GUNCELLENMIYOR
+    // setStudentList(newStudentList);
+
+    //DOGRU
+    // const newStudentList = [...studentList, newStudent];
+    // setStudentList(newStudentList);]
+    // setStudentList([...studentList, newStudent])
+    // setStudentList(
+    //   [
+    //     ...studentList,
+    //     { ...student, id: Math.random().toString() }
+    //   ]
+    // )
+    setStudentList((prevStudentList) =>
+      [...prevStudentList,
+      { ...student, id: Math.random().toString() }]
+    )
+    setStudentList((prevStudentList) =>
+      [...prevStudentList,
+      { ...student, id: Math.random().toString() }]
+    )
+    setStudentList((prevStudentList) =>
+      [...prevStudentList,
+      { ...student, id: Math.random().toString() }]
+    )
+
+    // //inputlari temizle
+    // setStudentNameInput("");
+    // setCourseInput("");
+    // setInstructorInput("");
+    // // }
+    setStudent({ studentName: "", course: "", instructor: "" });
+  }
 
   return (
     /* JSX */
@@ -36,76 +89,51 @@ function App() {
           <input
             type="text"
             placeholder='Student Name'
-            value={studentNameInput} //input'un degeri state'den gelsin
-            onChange={(event) => setStudentNameInput(event.target.value)} //state'i bu inputun degeriyle guncelle
+            value={student.studentName} //input'un degeri state'den gelsin
+            onChange={(event) => setStudent({ ...student, studentName: event.target.value })} //state'i bu inputun degeriyle guncelle
           />
           {/* conditional rendering */}
-          {studentNameError && <p className="error input-error">Please enter a valid student name</p>}
+          {studentError.studentName && <p className="error input-error">Please enter a valid student name</p>}
           <br /><br />
         </div>
         <div className="input-control">
           <input
             type="text"
             placeholder='Course'
-            value={courseInput} onChange={(event) => setCourseInput(event.target.value)}
+            value={student.course} onChange={(event) => setStudent({ ...student, course: event.target.value })}
           />
-          {courseError && <p className="error input-error">Please enter a valid course</p>}
+          {studentError.course && <p className="error input-error">Please enter a valid course</p>}
           <br /><br />
         </div>
         <div className="input-control">
           <input
             type="text"
             placeholder='Instructor'
-            value={instructorInput}
-            onChange={(event) => setInstructorInput(event.target.value)}
+            value={student.instructor}
+            onChange={(event) => setStudent({ ...student, instructor: event.target.value })}
           />
-          {instructorError && <p className="error input-error">Please enter a valid instructor</p>}
+          {studentError.instructor && <p className="error input-error">Please enter a valid instructor</p>}
           <br /><br />
         </div>
         <input
           type="submit"
-          onClick={
-            (event) => {
-              event.preventDefault();
-
-              //clear previous errors
-              setStudentNameError(false);
-              setCourseError(false);
-              setInstructorError(false);
-
-              if (!studentNameInput.trim() || !courseInput.trim() || !instructorInput.trim()) {
-                !studentNameInput.trim() && setStudentNameError(true);
-                !courseInput.trim() && setCourseError(true);
-                !instructorInput.trim() && setInstructorError(true);
-                //else yerine early return pattern kullanilabilir
-                return;
-              }
-              // else {
-              // setStudentName(studentNameInput.trim());
-              // setCourse(courseInput.trim());
-              // setInstructor(instructorInput.trim());
-
-              //inputlari temizle
-              setStudentNameInput("");
-              setCourseInput("");
-              setInstructorInput("");
-              // }
-            }
-          }
+          onClick={addStudent}
         />
       </form>
       <div className="student-list">
         <h3>Students</h3>
-        {studentList.map((student) => (
-          <div className="student-card" key={student.id}>
-            <ul>
-              <li>{student.studentName}</li>
-              <li>{student.course}</li>
-              <li>{student.instructor}</li>
-            </ul>
-          </div>
-        ))}
-        
+        <div className="student-card">
+          {studentList.map(
+            ({ studentName, course, instructor, id }) =>
+            (<div className="student-card" key={id}>
+              <ul>
+                <li>{studentName}</li>
+                <li>{course}</li>
+                <li>{instructor}</li>
+              </ul>
+            </div>)
+          )}
+        </div>
       </div>
     </div>
   );
